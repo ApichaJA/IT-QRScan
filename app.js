7bff26217a0804a9211f6e4fe373c5f611a0274b
ApichaJA <junyatanakorn@gmail.com>
2020-06-16 19:39:35 +0700

    add Cookie

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var cookie = require('cookie-parser');
+const { Client } = require('pg');
+
 var app = express();
+
+app.use(cookie());
+
+const client = new Client({
+    user: "Japanapi",
+    password: "Japan62070216",
+    host: "localhost",
+    post: 5432,
+    database: "it_scanner"
+})
+
+client.connect()
+    .then(() => console.log("Connected"))
+    .then(() => client.query("select * from it_user"))
+    .then(results => console.table(results.rows))
+    .catch(e => console.log(e))
+    .finally(() => client.end())
 
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
@@ -7,8 +27,32 @@ app.use('/js', express.static(__dirname + '/js'));
 
 app.set('view engine', 'ejs');
 
-app.get('/:room', function(req, res) {
-    var data = { user: "Apicha" };
-    res.render('home', { room: req.params.room, data: data });
+app.get('/', function(req, res) {
+    if (req.cookies.myCookie == "") {
+        res.render('home', { checkStatus: "SignIn" });
+    } else {
+        res.render('home', { checkStatus: "CheckIn" });
+    }
 });
+
+app.get('/room/:room', function(req, res) {
+    res.render('home', { room: req.params.room });
+});
+
+app.get('/makeqr', function(req, res) {
+    res.render('makeqr');
+});
+
+
+//Set Cookie
+app.get('/createCk', function(req, res) {
+    res.cookie('myCookie', 'Japanapi62070216');
+    res.end('Create Cookie');
+})
+
+app.get('/deleteCk', function(req, res) {
+    res.clearCookie('myCookie');
+    res.end('Del Cookie');
+})
+
 app.listen(8080);
